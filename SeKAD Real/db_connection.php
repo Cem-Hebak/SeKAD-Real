76bade6f2d52390bfd066c3a7103e92f1383ef13
ILMAN_KHAIRI <147372988+Uso030614@users.noreply.github.com>
2024-12-10 10:03:51 +0800

    update coding

--- SeKAD Real/db_connection.php
+++ SeKAD Real/db_connection.php
@@ -1,13 +1,18 @@
 <?php
 // db.php
 $host = 'localhost';
-$dbname = 'admin';
-$username = 'root';
-$password = '';
+$db = 'admin1';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
